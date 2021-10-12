ea18dbc6ca4e8823fb3a69db4d706aa50751472c
Charly M <35889038+ACJF00@users.noreply.github.com>
2021-10-12 18:31:46 +0200

    mon premier commit

---
+++ backend/models/like.js
@@ -0,0 +1,106 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Like extends Model {
+    /** 
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+ models.User.belongsToMany(models.Message, {
+      through: models.Like,
+      foreignKey: 'userId',
+      otherKey: 'messagesId',
+    });
+
+    models.Message.belongsToMany(models.User, {
+      through: models.Like,
+      foreignKey: 'messagesId',
+      otherKey: 'userId',
+    });
+
+    models.Like.belongsTo(models.User, {
+      foreignKey: 'userId',
+      as: 'user',
+    });
+
+    models.Like.belongsTo(models.Message, {
+      foreignKey: 'messagesId',
+      as: 'message',
+    });
+  Like.init({
+    messagesId: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'Message',
+        key: 'id'
+      }
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'User',
+        key: 'id'
+      }
+    },
+    isLike: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Like',
+  });
+  return Like;
+};
+}
+}
+
+/*'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Like = sequelize.define('Like', {
+    messagesId: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'Message',
+        key: 'id'
+      }
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'User',
+        key: 'id'
+      }
+    },
+    isLike: DataTypes.INTEGER
+  }, {});
+  Like.associate = function(models) {
+    // associations can be defined here
+    models.User.belongsToMany(models.Message, {
+      through: models.Like,
+      foreignKey: 'userId',
+      otherKey: 'messagesId',
+    });
+
+    models.Message.belongsToMany(models.User, {
+      through: models.Like,
+      foreignKey: 'messagesId',
+      otherKey: 'userId',
+    });
+
+    models.Like.belongsTo(models.User, {
+      foreignKey: 'userId',
+      as: 'user',
+    });
+
+    models.Like.belongsTo(models.Message, {
+      foreignKey: 'messagesId',
+      as: 'message',
+    });
+  };
+  return Like;
+};
+*/
+ 
---
+++ backend/server.js
@@ -0,0 +1,25 @@
+// Imports
+const express = require('express');
+const bodyParser = require('body-parser');
+const apiRouter = require('./apiRouter').router;
+
+// Instanciation serveur
+const server = express();
+
+// Body-Parser config
+server.use(bodyParser.urlencoded({ extended: true }));
+server.use(bodyParser.json());
+
+// Configure routes
+
+server.get('/', function (req, res) {
+    res.setHeader('Content-Type', 'text/html');
+    res.status(200).send('<h1>Bienvenue sur mon serveur</h1>');
+});
+
+server.use('/api/', apiRouter);
+
+// Launch server
+server.listen(3000, function() {
+    console.log('Serveur en écoute');
+});
